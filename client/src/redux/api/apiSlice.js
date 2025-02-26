40a67a39d3b1857ab9446809d484951d47c3251f
Ravinder singh <negiravi451@gmail.com>
2025-02-26 11:07:29 +0530

    Orender setup

--- client/src/redux/api/apiSlice.js
+++ client/src/redux/api/apiSlice.js
@@ -4,6 +4,7 @@ export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth?.admin?.token || getState().auth?.token;
       if (token) {
